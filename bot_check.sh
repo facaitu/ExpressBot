93aa1366b7137cfffeb48dc3fb798087a5064d19
Benny <benny.think@gmail.com>
2017-12-22 15:05:31 +0800

    sample checker

--- bot_check.sh
+++ bot_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# check bot status and restart it if necessary.
-# Requires root privilege
-# This script is obsolete, use systemd `Restart` instead.
+# check status and restart it if necessary.
 
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+
+export TOKEN=''
 export DB_PATH='/home/ExpressBot/expressbot/bot.db'
 
 python /home/ExpressBot/expressbot/timer.py
