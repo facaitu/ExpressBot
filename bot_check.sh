b1a1abaa9c1713b2a5f4f3cc90fead7cd57e5ced
Benny <benny.think@gmail.com>
2017-12-22 15:13:28 +0800

    timer debug

--- bot_check.sh
+++ bot_check.sh
@@ -2,7 +2,6 @@
 # check status and restart it if necessary.
 
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-
 export TOKEN=''
 export DB_PATH='/home/ExpressBot/expressbot/bot.db'
 
