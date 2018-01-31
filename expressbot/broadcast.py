2f7adfe3d3668161efb70e10499981155a8cf591
Benny <benny.think@gmail.com>
2018-01-31 16:09:34 +0800

    undone job count

--- expressbot/broadcast.py
+++ expressbot/broadcast.py
@@ -21,9 +21,19 @@ def broadcast(msg):
         send_message(chat_id[0], msg)
 
 
+def get_undone_count():
+    cmd = 'SELECT * FROM job WHERE done=?'
+    user_list = db.select(cmd, (0,))
+
+    print('The undone job count is %s' % len(user_list))
+
+
 if __name__ == '__main__':
+
     if len(sys.argv) != 2:
         print('参数错误')
+    elif sys.argv[1] == '0':
+        get_undone_count()
     else:
         # running under windows needs decode('gbk')
         broadcast(sys.argv[1])
